8d0df9ffc63736b661dca717fa4bb11609423f9a
Carlos <carlos.escobar_uk@live.com>
2020-12-05 15:04:43 +0000

    Finished HTML ands CSS files
    Next is focuse on javaScript and database

--- js/app.js
+++ js/app.js
@@ -7,31 +7,39 @@ const date = (document.getElementById(
 // ********** nav toggle ************
 // select button and links
 const navBtn = document.getElementById('nav-toggle');
-const links = document.getElementById('nav-links');
+const sideMenu = document.getElementById('side-menu');
+const sideMenuCloseBtn = document.getElementById('drawer-close');
+
+const cartDrawerBtn = document.getElementById('cart-drawer-toggle');
+const cartDrawer = document.getElementById('cart-drawer');
+const cartDrawerCloseBtn = document.getElementById('cart-drawer-close');
+
+const toggleFilterBt = document.getElementById('toggle-category-filters');
+const categoriesHolder = document.getElementById('categories-holder');
+const filterText = document.getElementById('filter-btn-span');
+
 // add event listener
 navBtn.addEventListener('click', () => {
-	links.classList.toggle('show-links');
+	sideMenu.classList.add('open');
 });
 
-// ********** smooth scroll ************
-// select links
-const scrollLinks = document.querySelectorAll('.scroll-link');
-scrollLinks.forEach((link) => {
-	link.addEventListener('click', (e) => {
-		// prevent default
-		e.preventDefault();
-		links.classList.remove('show-links');
-
-		const id = e.target.getAttribute('href').slice(1);
-		const element = document.getElementById(id);
-		//
-		let position = element.offsetTop - 62;
-
-		window.scrollTo({
-			left: 0,
-			// top: element.offsetTop,
-			top: position,
-			behavior: 'smooth',
-		});
-	});
+sideMenuCloseBtn.addEventListener('click', () => {
+	sideMenu.classList.remove('open');
 });
+
+cartDrawerBtn.addEventListener('click', () => {
+	cartDrawer.classList.add('cart-open');
+});
+
+cartDrawerCloseBtn.addEventListener('click', () => {
+	cartDrawer.classList.remove('cart-open');
+});
+
+toggleFilterBt.addEventListener('click', () => {
+	categoriesHolder.classList.toggle('categories-show');
+	filterText.innerHTML = categoriesHolder.classList.contains('categories-show')
+		? '<i class="far fa-plus-square"></i>'
+		: '<i class="far fa-minus-square"></i>';
+});
+
+// <i class="far fa-plus-square"></i> for filter
